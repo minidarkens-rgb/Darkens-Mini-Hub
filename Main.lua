80284403dbdc07f46650e58d5ce5fe5c3e8857da
minidarkens-rgb <minidarkens@gmail.com>
2026-07-16 15:21:01 -0400

    Update welcome message in Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,6 @@
-print("Bienvenido a Darkens Mini Hub")
+print("Darkens Mini Hub iniciado")
+
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+print("Bienvenido, " .. player.Name)
